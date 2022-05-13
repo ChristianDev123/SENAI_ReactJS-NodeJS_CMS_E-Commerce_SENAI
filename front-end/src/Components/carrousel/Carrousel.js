da29ddb89b32676ce35fed49893019801cf835ba
ChrisDev123 <christian.lima999@gmail.com>
2022-05-13 12:27:45 -0300

    Carousel Login Ready

--- front-end/src/Components/carrousel/Carrousel.js
+++ front-end/src/Components/carrousel/Carrousel.js
@@ -2,9 +2,20 @@ import { CarrouselWrapper, Wrapper , Image } from "./StyledCass";
 import { useState } from "react";
 import DarkIcon from '../../assets/MoonIcon.png';
 import LightIcon from '../../assets/SunIcon.svg';
+import { UncontrolledCarousel } from 'reactstrap';
+import GenericImage1 from '../../assets/GenericImage1.jpg';
+import GenericImage2 from '../../assets/GenericImage2.jpg';
+import GenericImage3 from '../../assets/GenericImage3.jpg';
+
 
 export default function LeftMain({changeTheme,currentTheme }){
     const [isDark,setIsDark] = useState(true);
+    const [imagesCarousel,setImagesCarousel] = useState([
+        {src:GenericImage1},
+        {src:GenericImage2},
+        {src:GenericImage3}
+    ]);
+    /* sempre usar imagens em modo retrato (3000,2000) */
     return(
         <>
             <Wrapper>
@@ -15,7 +26,7 @@ export default function LeftMain({changeTheme,currentTheme }){
                     }}
                 />
                 <CarrouselWrapper>
-                    asdasdasda
+                    <UncontrolledCarousel items={imagesCarousel}/>
                 </CarrouselWrapper>
             </Wrapper>
         </>
