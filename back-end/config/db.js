0f9684cb05759f1c193869b21d15b1bf84a208ae
ChristianDev123 <christian.lima999@gmail.com>
2022-05-30 09:48:01 -0300

    teste

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -3,5 +3,6 @@ const Sequelize = require('sequelize')
 const connectionDB = process.env.DATABASE_URL;
 
 const sequelize = new Sequelize(connectionDB);
+console.log("leu isso")
 
 module.exports = sequelize;
