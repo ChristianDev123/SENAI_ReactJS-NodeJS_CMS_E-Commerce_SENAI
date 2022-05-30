4cd730d943c3ea274bed570dc6faca041e45fd9e
ChristianDev123 <christian.lima999@gmail.com>
2022-05-30 10:15:04 -0300

    teste

--- back-end/config/dbMysql.js
+++ back-end/config/dbMysql.js
@@ -3,5 +3,6 @@ const {Client} = require('pg');
 const client = new Client(process.env.DATABASE_URL);
 const dbPostgres = client.connect();
 
+console.log(process.env.DATABASE_URL);
 
 module.exports = dbPostgres;
