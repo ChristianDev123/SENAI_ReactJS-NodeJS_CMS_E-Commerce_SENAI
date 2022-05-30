f4fde3fb3a2e69cebfe3a6e31c109dbd70dd4211
ChristianDev123 <christian.lima999@gmail.com>
2022-05-30 18:52:17 -0300

    Teste Sequelize Postgres

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize')
 
 const connectionDB = process.env.DATABASE_URL;
-    const sequelize = new Sequelize(process.env.DATABASE_URL,{
+    const sequelize = new Sequelize(connectionDB,{
         ssl:{rejectUnauthorized: false},
         dialect:"postgres"
     });
-
+    console.log(conectionDB)
 module.exports = sequelize;
