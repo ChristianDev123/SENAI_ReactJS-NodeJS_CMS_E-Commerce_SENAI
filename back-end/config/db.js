5ef5d17618fe39ef77cd69b0b8014e4a6b5ad339
ChristianDev123 <christian.lima999@gmail.com>
2022-05-30 10:45:39 -0300

    config sequelize to postgres

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize')
 
 const connectionDB = process.env.DATABASE_URL;
-
-const sequelize = new Sequelize(connectionDB);
+    const sequelize = new Sequelize(connectionDB,{ssl:{rejectUnauthorized: false}});
 
 module.exports = sequelize;
