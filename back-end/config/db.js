2d4284200b90848d4de997366443be30da637506
ChristianDev123 <christian.lima999@gmail.com>
2022-05-30 09:43:07 -0300

    Change Port DB

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -2,11 +2,6 @@ const Sequelize = require('sequelize')
 
 const connectionDB = process.env.DATABASE_URL;
 
-let sequelize;
-if(connectionDB){
-    sequelize = new Sequelize(process.env.DATABASE_URL,{dialect:"postgres"});
-}else{
-    sequelize = new Sequelize('cms_crud','root','',{dialect:'mysql',host:'localhost',port:3306});
-}
+const sequelize = new Sequelize(connectionDB);
 
 module.exports = sequelize;
