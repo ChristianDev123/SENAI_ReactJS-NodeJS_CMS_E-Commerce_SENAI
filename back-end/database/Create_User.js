3413d1549956dd381fa420f57eda5ca74cf51fbd
ChristianDev123 <christian.lima999@gmail.com>
2022-05-30 22:59:23 -0300

    Testes Querys

--- back-end/database/Create_User.js
+++ back-end/database/Create_User.js
@@ -2,7 +2,6 @@ const table = require('../models/User');
 
 const new_User = async(dados)=>{
     await table.create({
-        id:"null",
         usuario:dados.user,
         senha: dados.pass,
         matricula: dados.mat
