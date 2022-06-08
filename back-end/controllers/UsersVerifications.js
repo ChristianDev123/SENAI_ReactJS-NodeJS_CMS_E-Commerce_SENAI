4e209de3412a60e8034b7e4f67f5f576f635d624
ChristianDev123 <christian.lima999@gmail.com>
2022-06-08 12:14:07 -0300

    Fix Password

--- back-end/controllers/UsersVerifications.js
+++ back-end/controllers/UsersVerifications.js
@@ -14,7 +14,7 @@ class Verifications {
                 matricula: dados.mat
             }
         });
-        console.log(verifier)
+        console.log(verifier.senha)
         bcrypt.compare(dados.pass,verifier.Users.senha,(err,answer)=>{
             if(err) console.log(err);
             UserExists = answer
